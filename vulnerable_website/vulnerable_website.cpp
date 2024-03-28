298ea157dd46d7b4ef5dc2b4b7c15059646fe231
Ashley Jones <ashley.jones@robotiz3d.com>
2024-03-28 14:58:08 +0000

    Initial visual studio files

---
+++ vulnerable_website/vulnerable_website.cpp
@@ -0,0 +1,12 @@
+﻿// vulnerable_website.cpp : Defines the entry point for the application.
+//
+
+#include "vulnerable_website.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
